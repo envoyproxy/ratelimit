ba332ef93c80bc57c7f506fde6ba8dbe8a7a9d1e
Daniel Hochman <danielhochman@users.noreply.github.com>
2017-04-04 15:48:32 -0700

    gofmt check in travis (#10)

--- src/redis/cache.go
+++ src/redis/cache.go
@@ -12,8 +12,11 @@ type TimeSource interface {
 	UnixNow() int64
 }
 
+// Interface for a rand Source for expiration jitter.
 type JitterRandSource interface {
+	// @return a non-negative pseudo-random 63-bit integer as an int64.
 	Int63() int64
+	// @param seed initializes pseudo-random generator to a deterministic state.
 	Seed(seed int64)
 }
 
@@ -27,7 +30,7 @@ type RateLimitCache interface {
 	//               is done for simplicity reasons in the overall service API. The length of this
 	//               list must be same as the length of the descriptors list.
 	// @return a list of DescriptorStatuses which corresponds to each passed in descriptor/limit pair.
-	// Throws RedisError if there was any error talking to the cache.
+	// 				 Throws RedisError if there was any error talking to the cache.
 	DoLimit(
 		ctx context.Context,
 		request *pb.RateLimitRequest,
