4a33c42b04dda9c9dbce46e46e4b59fc67e47523
Daniel Hochman <dhochman@lyft.com>
2017-04-04 15:25:35 -0700

    more periods

--- src/redis/cache.go
+++ src/redis/cache.go
@@ -14,9 +14,9 @@ type TimeSource interface {
 
 // Interface for a rand Source for expiration jitter.
 type JitterRandSource interface {
-	// @return a non-negative pseudo-random 63-bit integer as an int64
+	// @return a non-negative pseudo-random 63-bit integer as an int64.
 	Int63() int64
-	// @param seed initializes pseudo-random generator to a deterministic state
+	// @param seed initializes pseudo-random generator to a deterministic state.
 	Seed(seed int64)
 }
 
