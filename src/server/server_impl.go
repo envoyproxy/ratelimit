0ded92a2af8261d43096eba4132e45b99a3b8b14
Jose Ulises Nino Rivera <junr03@users.noreply.github.com>
2018-03-16 12:20:29 -0700

    update dependencies (#35)
    * update dependencies

--- src/server/server_impl.go
+++ src/server/server_impl.go
@@ -14,12 +14,12 @@ import (
 
 	"net"
 
-	logger "github.com/Sirupsen/logrus"
 	"github.com/gorilla/mux"
 	"github.com/kavu/go_reuseport"
 	"github.com/lyft/goruntime/loader"
 	"github.com/lyft/gostats"
 	"github.com/lyft/ratelimit/src/settings"
+	logger "github.com/sirupsen/logrus"
 	"google.golang.org/grpc"
 )
 
@@ -121,7 +121,7 @@ func newServer(name string, opts ...settings.Option) *server {
 	ret.store.AddStatGenerator(stats.NewRuntimeStats(ret.scope.Scope("go")))
 
 	// setup runtime
-	ret.runtime = loader.New(s.RuntimePath, s.RuntimeSubdirectory, ret.store.Scope("runtime"))
+	ret.runtime = loader.New(s.RuntimePath, s.RuntimeSubdirectory, ret.store.Scope("runtime"), &loader.SymlinkRefresher{s.RuntimePath})
 
 	// setup http router
 	ret.router = mux.NewRouter()
