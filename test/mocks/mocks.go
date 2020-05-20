a532854440bbe8906154010b2501294e0b97c66b
Tong Cai <caitong93@gmail.com>
2020-05-20 15:42:27 +0800

    Go generate mocks
    Signed-off-by: Tong Cai <caitong93@gmail.com>

--- test/mocks/mocks.go
+++ test/mocks/mocks.go
@@ -3,4 +3,4 @@ package mocks
 //go:generate mockgen -destination ./runtime/snapshot/snapshot.go github.com/lyft/goruntime/snapshot IFace
 //go:generate mockgen -destination ./runtime/loader/loader.go github.com/lyft/goruntime/loader IFace
 //go:generate mockgen -destination ./config/config.go github.com/envoyproxy/ratelimit/src/config RateLimitConfig,RateLimitConfigLoader
-//go:generate mockgen -destination ./redis/redis.go github.com/envoyproxy/ratelimit/src/redis RateLimitCache,Pool,Connection,Response,TimeSource,JitterRandSource
+//go:generate mockgen -destination ./redis/redis.go github.com/envoyproxy/ratelimit/src/redis RateLimitCache,Client,TimeSource,JitterRandSource
