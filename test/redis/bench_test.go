b7dee4d69a425be056c8d5b8f171d0a48719c480
Tong Cai <caitong93@gmail.com>
2020-05-27 23:05:39 +0800

    Add more cases in  BenchmarkParallelDoLimit
    Signed-off-by: Tong Cai <caitong93@gmail.com>

--- test/redis/bench_test.go
+++ test/redis/bench_test.go
@@ -21,6 +21,7 @@ func BenchmarkParallelDoLimit(b *testing.B) {
 
 	b.ReportAllocs()
 
+	// See https://github.com/mediocregopher/radix/blob/v3.5.1/bench/bench_test.go#L176
 	parallel := runtime.GOMAXPROCS(0)
 	poolSize := parallel * runtime.GOMAXPROCS(0)
 
@@ -64,10 +65,29 @@ func BenchmarkParallelDoLimit(b *testing.B) {
 	}
 
 	b.Run("no pipeline", mkDoLimitBench(0, 0))
-	b.Run("pipeline 50us 4", mkDoLimitBench(50*time.Microsecond, 4))
-	b.Run("pipeline 75us  4", mkDoLimitBench(75*time.Microsecond, 4))
+
+	b.Run("pipeline 35us 1", mkDoLimitBench(35*time.Microsecond, 1))
+	b.Run("pipeline 75us 1", mkDoLimitBench(75*time.Microsecond, 1))
+	b.Run("pipeline 150us 1", mkDoLimitBench(150*time.Microsecond, 1))
+	b.Run("pipeline 300us 1", mkDoLimitBench(300*time.Microsecond, 1))
+
+	b.Run("pipeline 35us 2", mkDoLimitBench(35*time.Microsecond, 2))
+	b.Run("pipeline 75us 2", mkDoLimitBench(75*time.Microsecond, 2))
+	b.Run("pipeline 150us 2", mkDoLimitBench(150*time.Microsecond, 2))
+	b.Run("pipeline 300us 2", mkDoLimitBench(300*time.Microsecond, 2))
+
+	b.Run("pipeline 35us 4", mkDoLimitBench(35*time.Microsecond, 4))
+	b.Run("pipeline 75us 4", mkDoLimitBench(75*time.Microsecond, 4))
 	b.Run("pipeline 150us 4", mkDoLimitBench(150*time.Microsecond, 4))
-	b.Run("pipeline 50us 8", mkDoLimitBench(50*time.Microsecond, 8))
+	b.Run("pipeline 300us 4", mkDoLimitBench(300*time.Microsecond, 4))
+
+	b.Run("pipeline 35us 8", mkDoLimitBench(35*time.Microsecond, 8))
 	b.Run("pipeline 75us 8", mkDoLimitBench(75*time.Microsecond, 8))
 	b.Run("pipeline 150us 8", mkDoLimitBench(150*time.Microsecond, 8))
+	b.Run("pipeline 300us 8", mkDoLimitBench(300*time.Microsecond, 8))
+
+	b.Run("pipeline 35us 16", mkDoLimitBench(35*time.Microsecond, 16))
+	b.Run("pipeline 75us 16", mkDoLimitBench(75*time.Microsecond, 16))
+	b.Run("pipeline 150us 16", mkDoLimitBench(150*time.Microsecond, 16))
+	b.Run("pipeline 300us 16", mkDoLimitBench(300*time.Microsecond, 16))
 }
