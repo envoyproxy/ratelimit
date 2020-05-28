679fd0f3cf501b2b06ac026aefe9d0132b8b6bce
Tong Cai <caitong93@gmail.com>
2020-05-28 21:28:26 +0800

    Fix merge
    Signed-off-by: Tong Cai <caitong93@gmail.com>

--- test/mocks/mocks.go
+++ test/mocks/mocks.go
@@ -1,6 +1,6 @@
 package mocks
 
-//go:generate github.com/golang/mock/mockgen -destination ./runtime/snapshot/snapshot.go github.com/lyft/goruntime/snapshot IFace
-//go:generate github.com/golang/mock/mockgen -destination ./runtime/loader/loader.go github.com/lyft/goruntime/loader IFace
-//go:generate github.com/golang/mock/mockgen -destination ./config/config.go github.com/envoyproxy/ratelimit/src/config RateLimitConfig,RateLimitConfigLoader
-//go:generate github.com/golang/mock/mockgen -destination ./redis/redis.go github.com/envoyproxy/ratelimit/src/redis RateLimitCache,Client,TimeSource,JitterRandSource
+//go:generate go run github.com/golang/mock/mockgen -destination ./runtime/snapshot/snapshot.go github.com/lyft/goruntime/snapshot IFace
+//go:generate go run github.com/golang/mock/mockgen -destination ./runtime/loader/loader.go github.com/lyft/goruntime/loader IFace
+//go:generate go run github.com/golang/mock/mockgen -destination ./config/config.go github.com/envoyproxy/ratelimit/src/config RateLimitConfig,RateLimitConfigLoader
+//go:generate go run github.com/golang/mock/mockgen -destination ./redis/redis.go github.com/envoyproxy/ratelimit/src/redis RateLimitCache,Client,TimeSource,JitterRandSource
