4603f6c30f1288ed95adbb5f51bd8caa9141942c
ZhiYong Dai <dzy176@gmail.com>
2021-02-07 07:16:00 +0800

    client supports handling multi descriptors (#207)
    Signed-off-by: zhiyong.dai <zhiyong.dai@ucloud.cn>
    Signed-off-by: Yuki Sawa <yukisawa@gmail.com>

--- src/client_cmd/main.go
+++ src/client_cmd/main.go
@@ -13,42 +13,50 @@ import (
 	"google.golang.org/grpc"
 )
 
-type descriptorValue struct {
-	descriptor *pb_struct.RateLimitDescriptor
+type descriptorsValue struct {
+	descriptors []*pb_struct.RateLimitDescriptor
 }
 
-func (this *descriptorValue) Set(arg string) error {
+func (this *descriptorsValue) Set(arg string) error {
 	pairs := strings.Split(arg, ",")
-	for _, pair := range pairs {
+	entries := make([]*pb_struct.RateLimitDescriptor_Entry, len(pairs))
+	for i, pair := range pairs {
 		parts := strings.Split(pair, "=")
 		if len(parts) != 2 {
 			return errors.New("invalid descriptor list")
 		}
-
-		this.descriptor.Entries = append(
-			this.descriptor.Entries, &pb_struct.RateLimitDescriptor_Entry{Key: parts[0], Value: parts[1]})
+		entries[i] = &pb_struct.RateLimitDescriptor_Entry{Key: parts[0], Value: parts[1]}
 	}
+	this.descriptors = append(this.descriptors, &pb_struct.RateLimitDescriptor{Entries: entries})
 
 	return nil
 }
 
-func (this *descriptorValue) String() string {
-	return this.descriptor.String()
+func (this *descriptorsValue) String() string {
+	ret := ""
+	for _, descriptor := range this.descriptors {
+		tmp := ""
+		for _, entry := range descriptor.Entries {
+			tmp += fmt.Sprintf(" <key=%s, value=%s> ", entry.Key, entry.Value)
+		}
+		ret += fmt.Sprintf("[%s] ", tmp)
+	}
+	return ret
 }
 
 func main() {
 	dialString := flag.String(
 		"dial_string", "localhost:8081", "url of ratelimit server in <host>:<port> form")
 	domain := flag.String("domain", "", "rate limit configuration domain to query")
-	descriptorValue := descriptorValue{&pb_struct.RateLimitDescriptor{}}
+	descriptorsValue := descriptorsValue{[]*pb_struct.RateLimitDescriptor{}}
 	flag.Var(
-		&descriptorValue, "descriptors",
+		&descriptorsValue, "descriptors",
 		"descriptor list to query in <key>=<value>,<key>=<value>,... form")
 	flag.Parse()
 
 	fmt.Printf("dial string: %s\n", *dialString)
 	fmt.Printf("domain: %s\n", *domain)
-	fmt.Printf("descriptors: %s\n", &descriptorValue)
+	fmt.Printf("descriptors: %s\n", &descriptorsValue)
 
 	conn, err := grpc.Dial(*dialString, grpc.WithInsecure())
 	if err != nil {
@@ -58,11 +66,15 @@ func main() {
 
 	defer conn.Close()
 	c := pb.NewRateLimitServiceClient(conn)
+	desc := make([]*pb_struct.RateLimitDescriptor, len(descriptorsValue.descriptors))
+	for i, v := range descriptorsValue.descriptors {
+		desc[i] = v
+	}
 	response, err := c.ShouldRateLimit(
 		context.Background(),
 		&pb.RateLimitRequest{
 			Domain:      *domain,
-			Descriptors: []*pb_struct.RateLimitDescriptor{descriptorValue.descriptor},
+			Descriptors: desc,
 			HitsAddend:  1,
 		})
 	if err != nil {
