19f871edc111b2d38e6c8cd35b9f71d77e965e20
David Weitzman <dweitzman@pinterest.com>
2021-02-06 11:01:47 -0800

    Add EXTRA_TAGS config option to tag all stats (#220)
    This is useful for tagging all stats with things like build commit or host name.
    
    Additionally:
    - Reformat settings.go to make merge conflicts less common
    - Factor out some common logic in integration_test.go
    - Make integration_test.go slightly smarter & faster about
      waiting for test instances to come up instead of always waiting
      a full second.
    - Upgrades the envconfig library to a version that supports map configs
    
    Signed-off-by: David Weitzman <dweitzman@pinterest.com>

--- src/settings/settings.go
+++ src/settings/settings.go
@@ -11,18 +11,34 @@ type Settings struct {
 	// runtime options
 	GrpcUnaryInterceptor grpc.ServerOption
 	// env config
-	Port                         int           `envconfig:"PORT" default:"8080"`
-	GrpcPort                     int           `envconfig:"GRPC_PORT" default:"8081"`
-	DebugPort                    int           `envconfig:"DEBUG_PORT" default:"6070"`
-	UseStatsd                    bool          `envconfig:"USE_STATSD" default:"true"`
-	StatsdHost                   string        `envconfig:"STATSD_HOST" default:"localhost"`
-	StatsdPort                   int           `envconfig:"STATSD_PORT" default:"8125"`
-	RuntimePath                  string        `envconfig:"RUNTIME_ROOT" default:"/srv/runtime_data/current"`
-	RuntimeSubdirectory          string        `envconfig:"RUNTIME_SUBDIRECTORY"`
-	RuntimeIgnoreDotFiles        bool          `envconfig:"RUNTIME_IGNOREDOTFILES" default:"false"`
-	RuntimeWatchRoot             bool          `envconfig:"RUNTIME_WATCH_ROOT" default:"true"`
-	LogLevel                     string        `envconfig:"LOG_LEVEL" default:"WARN"`
-	LogFormat                    string        `envconfig:"LOG_FORMAT" default:"text"`
+	Port      int `envconfig:"PORT" default:"8080"`
+	GrpcPort  int `envconfig:"GRPC_PORT" default:"8081"`
+	DebugPort int `envconfig:"DEBUG_PORT" default:"6070"`
+
+	// Logging settings
+	LogLevel  string `envconfig:"LOG_LEVEL" default:"WARN"`
+	LogFormat string `envconfig:"LOG_FORMAT" default:"text"`
+
+	// Stats-related settings
+	UseStatsd  bool              `envconfig:"USE_STATSD" default:"true"`
+	StatsdHost string            `envconfig:"STATSD_HOST" default:"localhost"`
+	StatsdPort int               `envconfig:"STATSD_PORT" default:"8125"`
+	ExtraTags  map[string]string `envconfig:"EXTRA_TAGS" default:""`
+
+	// Settings for rate limit configuration
+	RuntimePath           string `envconfig:"RUNTIME_ROOT" default:"/srv/runtime_data/current"`
+	RuntimeSubdirectory   string `envconfig:"RUNTIME_SUBDIRECTORY"`
+	RuntimeIgnoreDotFiles bool   `envconfig:"RUNTIME_IGNOREDOTFILES" default:"false"`
+	RuntimeWatchRoot      bool   `envconfig:"RUNTIME_WATCH_ROOT" default:"true"`
+
+	// Settings for all cache types
+	ExpirationJitterMaxSeconds int64   `envconfig:"EXPIRATION_JITTER_MAX_SECONDS" default:"300"`
+	LocalCacheSizeInBytes      int     `envconfig:"LOCAL_CACHE_SIZE_IN_BYTES" default:"0"`
+	NearLimitRatio             float32 `envconfig:"NEAR_LIMIT_RATIO" default:"0.8"`
+	CacheKeyPrefix             string  `envconfig:"CACHE_KEY_PREFIX" default:""`
+	BackendType                string  `envconfig:"BACKEND_TYPE" default:"redis"`
+
+	// Redis settings
 	RedisSocketType              string        `envconfig:"REDIS_SOCKET_TYPE" default:"unix"`
 	RedisType                    string        `envconfig:"REDIS_TYPE" default:"SINGLE"`
 	RedisUrl                     string        `envconfig:"REDIS_URL" default:"/var/run/nutcracker/ratelimit.sock"`
@@ -40,12 +56,9 @@ type Settings struct {
 	RedisPerSecondTls            bool          `envconfig:"REDIS_PERSECOND_TLS" default:"false"`
 	RedisPerSecondPipelineWindow time.Duration `envconfig:"REDIS_PERSECOND_PIPELINE_WINDOW" default:"0"`
 	RedisPerSecondPipelineLimit  int           `envconfig:"REDIS_PERSECOND_PIPELINE_LIMIT" default:"0"`
-	ExpirationJitterMaxSeconds   int64         `envconfig:"EXPIRATION_JITTER_MAX_SECONDS" default:"300"`
-	LocalCacheSizeInBytes        int           `envconfig:"LOCAL_CACHE_SIZE_IN_BYTES" default:"0"`
-	NearLimitRatio               float32       `envconfig:"NEAR_LIMIT_RATIO" default:"0.8"`
-	MemcacheHostPort             string        `envconfig:"MEMCACHE_HOST_PORT" default:""`
-	BackendType                  string        `envconfig:"BACKEND_TYPE" default:"redis"`
-	CacheKeyPrefix               string        `envconfig:"CACHE_KEY_PREFIX" default:""`
+
+	// Memcache settings
+	MemcacheHostPort string `envconfig:"MEMCACHE_HOST_PORT" default:""`
 }
 
 type Option func(*Settings)
