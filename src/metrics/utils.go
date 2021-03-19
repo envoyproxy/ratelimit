a36be1387cbf206c1f9d229c184d3e0a55e3dd97
Terry <76925935+thao-wish@users.noreply.github.com>
2021-03-19 13:55:51 -0400

    Add server-side metrics (#236)
    Signed-off-by: Jia Hao <thao@wish.com>
    Signed-off-by: Yuki Sawa <yukisawa@gmail.com>

---
+++ src/metrics/utils.go
@@ -0,0 +1,11 @@
+package metrics
+
+import "strings"
+
+func splitMethodName(fullMethodName string) (string, string) {
+	fullMethodName = strings.TrimPrefix(fullMethodName, "/") // remove leading slash
+	if i := strings.Index(fullMethodName, "/"); i >= 0 {
+		return fullMethodName[:i], fullMethodName[i+1:]
+	}
+	return "unknown", "unknown"
+}
