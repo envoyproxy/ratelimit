830d433472605a42c2ba413975090a3af2ffb3e2
Terry <76925935+thao-wish@users.noreply.github.com>
2021-03-19 13:55:51 -0400

    Add server-side metrics (#236)
    Signed-off-by: Jia Hao <thao@wish.com>

---
+++ src/metrics/metrics.go
@@ -0,0 +1,45 @@
+package metrics
+
+import (
+	"context"
+	stats "github.com/lyft/gostats"
+	"google.golang.org/grpc"
+	"time"
+)
+
+type serverMetrics struct {
+	totalRequests stats.Counter
+	responseTime  stats.Timer
+}
+
+// ServerReporter reports server-side metrics for ratelimit gRPC server
+type ServerReporter struct {
+	scope stats.Scope
+}
+
+func newServerMetrics(scope stats.Scope, fullMethod string) *serverMetrics {
+	_, methodName := splitMethodName(fullMethod)
+	ret := serverMetrics{}
+	ret.totalRequests = scope.NewCounter(methodName + ".total_requests")
+	ret.responseTime = scope.NewTimer(methodName + ".response_time")
+	return &ret
+}
+
+// NewServerReporter returns a ServerReporter object.
+func NewServerReporter(scope stats.Scope) *ServerReporter {
+	return &ServerReporter{
+		scope: scope,
+	}
+}
+
+// UnaryServerInterceptor is a gRPC server-side interceptor that provides server metrics for Unary RPCs.
+func (r *ServerReporter) UnaryServerInterceptor() func(ctx context.Context, req interface{}, info *grpc.UnaryServerInfo, handler grpc.UnaryHandler) (interface{}, error) {
+	return func(ctx context.Context, req interface{}, info *grpc.UnaryServerInfo, handler grpc.UnaryHandler) (interface{}, error) {
+		start := time.Now()
+		s := newServerMetrics(r.scope, info.FullMethod)
+		s.totalRequests.Inc()
+		resp, err := handler(ctx, req)
+		s.responseTime.AddValue(float64(time.Since(start).Milliseconds()))
+		return resp, err
+	}
+}
---
+++ test/metrics/metrics_test.go
@@ -0,0 +1,84 @@
+package metrics
+
+import (
+	"context"
+	"github.com/envoyproxy/ratelimit/src/metrics"
+	stats "github.com/lyft/gostats"
+	statsMock "github.com/lyft/gostats/mock"
+	"github.com/stretchr/testify/assert"
+	"google.golang.org/grpc"
+	"testing"
+	"time"
+)
+
+func TestMetricsInterceptor(t *testing.T) {
+	mockSink := statsMock.NewSink()
+	statsStore := stats.NewStore(mockSink, false)
+	serverReporter := metrics.NewServerReporter(statsStore)
+
+	unaryInfo := &grpc.UnaryServerInfo{
+		FullMethod: "TestService/TestMethod",
+	}
+	handler := func(ctx context.Context, req interface{}) (interface{}, error) {
+		time.Sleep(100 * time.Millisecond)
+		return req, nil
+	}
+
+	ctx := context.Background()
+	interceptor := serverReporter.UnaryServerInterceptor()
+
+	var iterations uint64 = 5
+
+	for i := uint64(0); i < iterations; i++ {
+		_, err := interceptor(ctx, nil, unaryInfo, handler)
+		assert.NoError(t, err)
+	}
+
+	totalRequestsCounter := statsStore.NewCounter("TestMethod.total_requests")
+	assert.Equal(t, iterations, totalRequestsCounter.Value())
+	assert.True(t, mockSink.Timer("TestMethod.response_time") >= float64(iterations*100))
+}
+
+func TestMetricsInterceptor_Concurrent(t *testing.T) {
+	mockSink := statsMock.NewSink()
+	statsStore := stats.NewStore(mockSink, false)
+	serverReporter := metrics.NewServerReporter(statsStore)
+
+	unaryInfo := &grpc.UnaryServerInfo{
+		FullMethod: "TestService/TestMethod",
+	}
+	handler := func(ctx context.Context, req interface{}) (interface{}, error) {
+		return req, nil
+	}
+
+	ctx := context.Background()
+	interceptor := serverReporter.UnaryServerInterceptor()
+
+	var iterations uint64 = 50
+	c := make(chan bool)
+
+	go func() {
+		for i := uint64(0); i < iterations; i++ {
+			_, err := interceptor(ctx, nil, unaryInfo, handler)
+			assert.NoError(t, err)
+		}
+		c <- true
+	}()
+
+	go func() {
+		for i := uint64(0); i < iterations; i++ {
+			_, err := interceptor(ctx, nil, unaryInfo, handler)
+			assert.NoError(t, err)
+		}
+		c <- true
+	}()
+
+	for i := 0; i < 2; i++ {
+		<-c
+	}
+
+	totalRequestsCounter := statsStore.NewCounter("TestMethod.total_requests")
+	assert.Equal(t, iterations*2, totalRequestsCounter.Value())
+	// verify that timer exists in the sink
+	assert.NotEqual(t, 0, mockSink.Timer("TestMethod.response_time"))
+}
