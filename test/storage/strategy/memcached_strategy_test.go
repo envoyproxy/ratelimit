c50480acbdc90d7f445594ee741f9423bd69ff43
zufardhiyaulhaq <zufardhiyaulhaq@gmail.com>
2021-05-31 17:22:33 +0200

    add storage strategy unit test
    Signed-off-by: zufardhiyaulhaq <zufardhiyaulhaq@gmail.com>

---
+++ test/storage/strategy/memcached_strategy_test.go
@@ -0,0 +1,65 @@
+package strategy_test
+
+import (
+	"strconv"
+	"testing"
+
+	"github.com/bradfitz/gomemcache/memcache"
+	"github.com/envoyproxy/ratelimit/src/storage/strategy"
+	mock_service "github.com/envoyproxy/ratelimit/test/mocks/storage/service"
+	"github.com/golang/mock/gomock"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestMemcachedStrategyGetValue(t *testing.T) {
+	assert := assert.New(t)
+	controller := gomock.NewController(t)
+	defer controller.Finish()
+
+	mockMemcachedClient := mock_service.NewMockMemcachedClientInterface(controller)
+	memcachedStrategy := strategy.MemcachedStrategy{
+		Client: mockMemcachedClient,
+	}
+
+	mockMemcachedClient.EXPECT().Get("key").Return(&memcache.Item{Key: "key", Value: []byte("5")}, nil)
+	value, err := memcachedStrategy.GetValue("key")
+
+	assert.Equal(value, uint64(5))
+	assert.Nil(err)
+}
+
+func TestMemcachedStrategySetValue(t *testing.T) {
+	assert := assert.New(t)
+	controller := gomock.NewController(t)
+	defer controller.Finish()
+
+	mockMemcachedClient := mock_service.NewMockMemcachedClientInterface(controller)
+	memcachedStrategy := strategy.MemcachedStrategy{
+		Client: mockMemcachedClient,
+	}
+
+	mockMemcachedClient.EXPECT().Set(&memcache.Item{
+		Key:        "key",
+		Value:      []byte(strconv.FormatUint(uint64(5), 10)),
+		Expiration: int32(5),
+	}).Return(nil)
+
+	err := memcachedStrategy.SetValue("key", uint64(5), uint64(5))
+	assert.Nil(err)
+}
+
+func TestMemcachedStrategyIncrementValue(t *testing.T) {
+	assert := assert.New(t)
+	controller := gomock.NewController(t)
+	defer controller.Finish()
+
+	mockMemcachedClient := mock_service.NewMockMemcachedClientInterface(controller)
+	memcachedStrategy := strategy.MemcachedStrategy{
+		Client: mockMemcachedClient,
+	}
+
+	mockMemcachedClient.EXPECT().Increment("key", uint64(1)).Return(uint64(1), nil)
+
+	err := memcachedStrategy.IncrementValue("key", uint64(1))
+	assert.Nil(err)
+}
---
+++ test/storage/strategy/redis_strategy_test.go
@@ -0,0 +1,71 @@
+package strategy_test
+
+import (
+	"testing"
+
+	"github.com/alicebob/miniredis"
+	"github.com/envoyproxy/ratelimit/src/storage/strategy"
+	mock_service "github.com/envoyproxy/ratelimit/test/mocks/storage/service"
+	"github.com/golang/mock/gomock"
+	"github.com/mediocregopher/radix/v3"
+	"github.com/stretchr/testify/assert"
+)
+
+func mustNewRedisServer() *miniredis.Miniredis {
+	srv, err := miniredis.Run()
+	if err != nil {
+		panic(err)
+	}
+
+	return srv
+}
+func TestRedisStrategyGetValue(t *testing.T) {
+	assert := assert.New(t)
+	controller := gomock.NewController(t)
+	defer controller.Finish()
+
+	mockRedisClient := mock_service.NewMockRedisClientInterface(controller)
+	redisStrategy := strategy.RedisStrategy{
+		Client: mockRedisClient,
+	}
+
+	var value uint64
+	mockRedisClient.EXPECT().Do(radix.Cmd(&value, "GET", "key")).Return(nil)
+	value, err := redisStrategy.GetValue("key")
+
+	assert.Equal(value, uint64(0))
+	assert.Nil(err)
+}
+
+func TestRedisStrategySetValue(t *testing.T) {
+	assert := assert.New(t)
+	controller := gomock.NewController(t)
+	defer controller.Finish()
+
+	mockRedisClient := mock_service.NewMockRedisClientInterface(controller)
+	redisStrategy := strategy.RedisStrategy{
+		Client: mockRedisClient,
+	}
+
+	mockRedisClient.EXPECT().Do(radix.FlatCmd(nil, "SET", "key", uint64(5))).Return(nil)
+	mockRedisClient.EXPECT().Do(radix.FlatCmd(nil, "EXPIRE", "key", uint64(5))).Return(nil)
+
+	err := redisStrategy.SetValue("key", uint64(5), uint64(5))
+	assert.Nil(err)
+}
+
+func TestRedisStrategyIncrementValue(t *testing.T) {
+	assert := assert.New(t)
+	controller := gomock.NewController(t)
+	defer controller.Finish()
+
+	mockRedisClient := mock_service.NewMockRedisClientInterface(controller)
+	redisStrategy := strategy.RedisStrategy{
+		Client: mockRedisClient,
+	}
+
+	mockRedisClient.EXPECT().Do(radix.FlatCmd(nil, "INCRBY", "key", uint64(1))).Return(nil)
+
+	err := redisStrategy.IncrementValue("key", uint64(1))
+	assert.Nil(err)
+}
