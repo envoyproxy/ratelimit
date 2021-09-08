5b63d8cac393d8b76d5b274afb391fb3c462e7ae
Javier Ruiz Arduengo <javier@squareup.com>
2021-09-08 13:31:46 -0400

    Fix formatting of settings.go
    Signed-off-by: Javier Ruiz Arduengo <javier@squareup.com>

--- src/settings/settings.go
+++ src/settings/settings.go
@@ -49,7 +49,8 @@ type Settings struct {
 	RedisPoolSize   int    `envconfig:"REDIS_POOL_SIZE" default:"10"`
 	RedisAuth       string `envconfig:"REDIS_AUTH" default:""`
 	RedisTls        bool   `envconfig:"REDIS_TLS" default:"false"`
-	RedisTlsConfig   *tls.Config
+	RedisTlsConfig  *tls.Config
+
 	// RedisPipelineWindow sets the duration after which internal pipelines will be flushed.
 	// If window is zero then implicit pipelining will be disabled. Radix use 150us for the
 	// default value, see https://github.com/mediocregopher/radix/blob/v3.5.1/pool.go#L278.
