f40e6b44203406425da6ff852d9ec9cf0d6a94b7
Chris Redekop <chris@replicon.com>
2022-09-01 22:16:31 -0600

    Explicitly run config-generator script in 'local' mode

--- sync_config.sh
+++ sync_config.sh
@@ -7,6 +7,7 @@ do
   /usr/local/bin/aws s3 sync --delete ${RLS_S3_PATH} data/
 
   [ $? -eq 0 ] && /usr/local/bin/python3 data/${RLS_SCRIPT_DIR}config-generator.py \
+    --no-docker-container \
     --static data/${RLS_STATIC_DIR} \
     --blacklist data/${RLS_BLACKLIST_DIR} \
     --override data/${RLS_OVERRIDE_DIR} \
