00d7d6cb12deef6652940ffc482df59cb28364dd
Josh <jjaques@gmail.com>
2024-06-20 10:53:31 -0500

    feat(dogstatsd_sink): support EXTRA_TAGS (#625)
    When using the godogstats sink, previously the EXTRA_TAGS would not be
    emitted as datadog tags.
    
    Signed-off-by: Josh Jaques <jjaques@gmail.com>

--- src/godogstats/dogstatsd_sink.go
+++ src/godogstats/dogstatsd_sink.go
@@ -3,10 +3,12 @@ package godogstats
 import (
 	"regexp"
 	"strconv"
+	"strings"
 	"time"
 
 	"github.com/DataDog/datadog-go/v5/statsd"
 	gostats "github.com/lyft/gostats"
+	logger "github.com/sirupsen/logrus"
 )
 
 type godogStatsSink struct {
@@ -65,18 +67,60 @@ func NewSink(opts ...goDogStatsSinkOption) (*godogStatsSink, error) {
 	return sink, nil
 }
 
-func (g *godogStatsSink) FlushCounter(name string, value uint64) {
+// separateTags separates the metric name and tags from the combined serialized metric name.
+// e.g. given input: "ratelimit.service.rate_limit.mongo_cps.database_users.total_hits.__COMMIT=12345.__DEPLOY=67890"
+// this should produce output: "ratelimit.service.rate_limit.mongo_cps.database_users.total_hits", ["COMMIT:12345", "DEPLOY:67890"]
+// Aligns to how tags are serialized here https://github.com/lyft/gostats/blob/49e70f1b7932d146fecd991be04f8e1ad235452c/internal/tags/tags.go#L335
+func separateTags(name string) (string, []string) {
+	const (
+		prefix = ".__"
+		sep    = "="
+	)
+
+	// split the name and tags about the first prefix for extra tags
+	shortName, tagString, hasTags := strings.Cut(name, prefix)
+	if !hasTags {
+		return name, nil
+	}
+
+	// split the tags at every instance of prefix
+	tagPairs := strings.Split(tagString, prefix)
+	tags := make([]string, 0, len(tagPairs))
+	for _, tagPair := range tagPairs {
+		// split the name + value by the seperator
+		tagName, tagValue, isValid := strings.Cut(tagPair, sep)
+		if !isValid {
+			logger.Debugf("godogstats sink found malformed extra tag: %v, string: %v", tagPair, name)
+			continue
+		}
+		tags = append(tags, tagName+":"+tagValue)
+	}
+
+	return shortName, tags
+}
+
+// mogrify takes a serialized metric name as input (internal gostats format)
+// and returns a metric name and list of tags (dogstatsd output format)
+// the output list of tags includes any "tags" that are serialized into the metric name,
+// as well as any other tags emitted by the mogrifier config
+func (g *godogStatsSink) mogrify(name string) (string, []string) {
+	name, extraTags := separateTags(name)
 	name, tags := g.mogrifier.mogrify(name)
+	return name, append(extraTags, tags...)
+}
+
+func (g *godogStatsSink) FlushCounter(name string, value uint64) {
+	name, tags := g.mogrify(name)
 	g.client.Count(name, int64(value), tags, 1.0)
 }
 
 func (g *godogStatsSink) FlushGauge(name string, value uint64) {
-	name, tags := g.mogrifier.mogrify(name)
+	name, tags := g.mogrify(name)
 	g.client.Gauge(name, float64(value), tags, 1.0)
 }
 
 func (g *godogStatsSink) FlushTimer(name string, milliseconds float64) {
-	name, tags := g.mogrifier.mogrify(name)
+	name, tags := g.mogrify(name)
 	duration := time.Duration(milliseconds) * time.Millisecond
 	g.client.Timing(name, duration, tags, 1.0)
 }
